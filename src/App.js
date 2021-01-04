18e4e74ef2219b8350dd582b042c04564bec82fa
Zain Sadaqat <zain.sadaqat.9798@gmail.com>
2021-01-04 22:25:30 +0500

    Added sign-in page and components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from './pages/homepage/homepage.component';
 import Shop from './pages/shop/shop.component';
 import {Switch, Route} from 'react-router-dom';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Switch>
         <Route exact={true} path='/' component={HomePage} />
         <Route exact={true} path='/shop' component={Shop} />
+        <Route exact={true} path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
